9ada26d8837e82cf96db187407e8c26d0f9d8623
ElAv252 <62076559+ElAv252@users.noreply.github.com>
2021-05-30 23:28:17 +0300

    Update Question.jsx

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,26 +1,5 @@
 import React from 'react';
 
-/*export default class Question extends React.Component {
-    constructor(props) {
-        super(props)
-        this.Questions = ['1*1', 'if 1*1 is 1, so how much is 2*2?', '3*4', '5*5'];
-
-    }
-
-    render() {
-        return (
-            <div className="MainFrame">
-                <div className="FrameQuestion">
-                    <p className="TheQuestion" >{this.Questions[this.props.index]}</p>
-                </div>
-                <div className="CorrectAnswer">
-                    <p></p>
-                </div>
-            </div>
-        )
-    }
-};*/
-
 export default function Question(props) {
 
     const Questions = ['1*1', 'if 1*1 is 1, so how much is 2*2?', '3*4', '5*5'];
@@ -30,9 +9,7 @@ export default function Question(props) {
             <div className="FrameQuestion">
                 <p className="TheQuestion" >{Questions[props.index]}</p>
             </div>
-            <div className="CorrectAnswer">
-                <p></p>
-            </div>
         </div>
     )
 };
+
