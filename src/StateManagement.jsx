bb432cfd914d1646d0e99c4ef5dc3b9603db0269
ElAv252 <62076559+ElAv252@users.noreply.github.com>
2021-06-09 01:47:52 +0300

    Update StateManagement.jsx

--- src/StateManagement.jsx
+++ src/StateManagement.jsx
@@ -2,25 +2,6 @@ import React, { useState } from 'react';
 import Question from './Question';
 import Answer from './Answer';
 
-/*export default class StateManagement extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { index: 0 }
-    }
-
-    setIndex = () => {
-        this.setState({ index: this.state.index + 1 })
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <Question index={this.state.index} />
-                <Answer index={this.state.index} setIndex={this.setIndex} />
-            </React.Fragment >
-        )
-    }*/
-
 export default function StateManagement() {
 
     const [index, setIndex] = useState(0);
